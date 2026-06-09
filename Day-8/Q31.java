24f57a3e9834393257a2f311e7b3f053bfb5f87c
Harshita <harshita93159@gmail.com>
2026-06-09 12:28:55 +0530

    Assignment is uploaded

---
+++ Day-8/Q31.java
@@ -0,0 +1,11 @@
+public class Q31 {
+    public static void main(String[] args) {
+        char ch='E';
+        for(char i='A';i<=ch;i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
