23333cc83563f48050075b8746eb56147d914e76
Harshita <harshita93159@gmail.com>
2026-06-09 12:11:29 +0530

    Assignment is uploaded

---
+++ Day-9/Q35.java
@@ -0,0 +1,11 @@
+public class Q35 {
+    public static void main(String[] args) {
+        char ch='E';
+        for(int i='A';i<=ch;i++){
+            for(int j='A';j<=i;j++){
+                System.out.print((char)i);
+            }
+            System.out.println();
+        }
+    }
+}
