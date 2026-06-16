a76c70f2f3f869f3b828b2aba1ff158d229f8792
Harshita <harshita93159@gmail.com>
2026-06-16 21:52:22 +0530

    Assignment is Uploaded

---
+++ Day-16/PairWithSum.java
@@ -0,0 +1,14 @@
+public class PairWithSum {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 7, 5, 1, 3};
+        int sum = 8;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == sum) {
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + sum);
+                }
+            }
+        }
+    }
+}
