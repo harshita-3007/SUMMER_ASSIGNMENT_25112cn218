0c0ed8d99314ee3953b15cd7e97070b42f622e7d
Harshita <harshita93159@gmail.com>
2026-06-19 21:13:31 +0530

    Assigment is Uploaded

---
+++ Day-19/MatrixTranspose.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class MatrixTranspose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[][] a = new int[r][c];
+
+        for(int i=0;i<r;i++)
+            for(int j=0;j<c;j++)
+                a[i][j] = sc.nextInt();
+
+        for(int i=0;i<c;i++) {
+            for(int j=0;j<r;j++)
+                System.out.print(a[j][i] + " ");
+            System.out.println();
+        }
+    }
+}
